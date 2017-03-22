a401e91d0751057154ddbc19a179f7bb8717a7fb
Matt Glover <matthew.glover@skybettingandgaming.com>
2017-03-22 07:20:47 +0000

    Initial Commit

---
+++ GithubStatusKit/GithubStatusKit.h
@@ -0,0 +1,19 @@
+//
+//  GithubStatusKit.h
+//  GithubStatusKit
+//
+//  Created by Glover, Matthew on 22/03/2017.
+//  Copyright © 2017 Glover, Matthew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GithubStatusKit.
+FOUNDATION_EXPORT double GithubStatusKitVersionNumber;
+
+//! Project version string for GithubStatusKit.
+FOUNDATION_EXPORT const unsigned char GithubStatusKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubStatusKit/PublicHeader.h>
+
+
