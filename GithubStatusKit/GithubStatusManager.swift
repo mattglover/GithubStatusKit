6a25452dec492856cb25056670985cad8808deee
Matt Glover <matt@duchysoftware.com>
2017-03-22 22:32:37 +0000

    Created GithubStatusManagerProtocol to help with testability Created subclass: MockGithubStatusManager to help verify method calls  and help testability

--- GithubStatusKit/GithubStatusManager.swift
+++ GithubStatusKit/GithubStatusManager.swift
@@ -1,11 +1,16 @@
 import Foundation
 
-struct GithubStatusManager {
-    
-    var environment: String!
-    
-    init(environment: String = "Production") {
-        self.environment = environment
-    }
-    
+class GithubStatusManager: GithubStatusManagerProtocol {
+
+	var availableMethods: [String:URL]?
+
+	init() {
+		fetchAvailableMethods { (availableMethods: [String : URL]) -> (Void) in
+			self.availableMethods = availableMethods
+		}
+	}
+
+	func fetchAvailableMethods(completion: ([String:URL]) -> (Void)) {
+		
+	}
 }
